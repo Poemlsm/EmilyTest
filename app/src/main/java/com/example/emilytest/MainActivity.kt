347121d28c622d44a4d8237f69ab6db061c74fe8
emily <1130901488@qq.com>
2020-03-09 10:46:52 +0800

    Bottom sheet

--- app/src/main/java/com/example/emilytest/MainActivity.kt
+++ app/src/main/java/com/example/emilytest/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.emilytest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<AppCompatButton>(R.id.control_button)
+        val bottomSheetBehavior =
+            BottomSheetBehavior.from(findViewById<LinearLayout>(R.id.bottom_sheet))
+
+        bottomSheetBehavior.peekHeight = 0
+        bottomSheetBehavior.skipCollapsed = false
+
+        button.setOnClickListener {
+            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            } else if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED) {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED;
+            }
+
+        }
     }
+
 }
